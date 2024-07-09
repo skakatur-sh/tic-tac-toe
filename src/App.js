0ee004d7af1cda606b3175a8224e24c6cd1e9cfc
skakatur <skakatur@sidecarhealth.com>
2024-07-09 12:01:20 -0700

    moved checkWin to seperate file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState } from 'react'
+import { checkWin } from './checkWin'
 
 const Square = ({value, onSquareClick}) => {
   return (
@@ -65,24 +66,24 @@ export function Board() {
   );
 }
 
-const checkWin = (squares) => {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
+// const checkWin = (squares) => {
+//   const lines = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6]
+//   ];
 
-  for (var i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] == squares[b] && squares[a] == squares[c]) {
-      return squares[a];
-    }
-  }
+//   for (var i = 0; i < lines.length; i++) {
+//     const [a, b, c] = lines[i];
+//     if (squares[a] && squares[a] == squares[b] && squares[a] == squares[c]) {
+//       return squares[a];
+//     }
+//   }
 
-  return null;
-}
+//   return null;
+// }
