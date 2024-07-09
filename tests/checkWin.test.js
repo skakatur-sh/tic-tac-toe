4aeee9849057a1c536f9a8ccc9ac76ef84f089d3
skakatur <skakatur@sidecarhealth.com>
2024-07-09 15:07:51 -0700

    mellowed out checkwin tests

--- tests/checkWin.test.js
+++ tests/checkWin.test.js
@@ -1,10 +1,23 @@
 const { checkWin } = require("../src/App");
 
-test('No win possible ', () => {
-    expect(checkWin(
-        ["X", "X", "O", 
-        "O", "X", "X", 
-        "X", "O", "O"]
-        )
-    ).toBe(null);
+describe("No Win Possible", () => {
+  test("Board Full ", () => {
+    expect(checkWin(["X", "X", "O", "O", "X", "X", "X", "O", "O"])).toBe(null);
+  });
+
+  test("Board Partial ", () => {
+    expect(checkWin(["X", "X", null, "O", null, "X", "X", "O", "O"])).toBe(
+      null
+    );
+  });
 });
+
+describe("Win Possible", () => {
+    test("Board Partial ", () => {
+      expect(checkWin(["X", "O", "X", "O", "X", "O", null, null, "X"])).toBe("X");
+    });
+  
+    test("Board Full ", () => {
+      expect(checkWin(["O", "O", "O", "X", "O", "X", "X", "O", "X"])).toBe("O");
+    });
+  });
